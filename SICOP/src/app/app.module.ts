d6c97cb1470f038f76f1bdd6a51156b100892760
dcastro18 <h.davidcastro14@gmail.com>
2022-03-18 16:31:50 -0600

    Login Component creation

--- SICOP/src/app/app.module.ts
+++ SICOP/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './Components/UserManagement/login-page/login-page.component';
+import { RegisterPageComponent } from './Components/UserManagement/register-page/register-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    RegisterPageComponent
   ],
   imports: [
     BrowserModule
