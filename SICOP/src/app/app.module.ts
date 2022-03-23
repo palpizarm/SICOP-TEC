2fb60707f7db660749665391f7a8a7d83a38a7a8
dcastro18 <h.davidcastro14@gmail.com>
2022-03-23 16:15:59 -0600

    Update app.module.ts

--- SICOP/src/app/app.module.ts
+++ SICOP/src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './Components/UserManagement/login-page/login-page.component';
-import { RegisterPageComponent } from './Components/UserManagement/register-page/register-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPageComponent,
-    RegisterPageComponent
+    LoginPageComponent
   ],
   imports: [
     BrowserModule
