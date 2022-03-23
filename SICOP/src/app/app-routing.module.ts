36b8915344a248bcb787a1c0ce42b09802eefb33
palpizarm <pabloalpizar99@gmail.com>
2022-03-23 00:17:45 -0600

    fixed issue with no existing import component

--- SICOP/src/app/app-routing.module.ts
+++ SICOP/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SideBarComponent } from './Components/side-bar/side-bar.component';
 
 const routes: Routes = [
 
