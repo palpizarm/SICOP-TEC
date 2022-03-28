e30982df572794e3ae08d66e2ee388dbb0f07eb5
palpizarm <pabloalpizar99@gmail.com>
2022-03-28 07:34:45 -0600

    fix coflicts

--- SICOP/src/app/app.module.ts
+++ SICOP/src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-<<<<<<< HEAD
 import { UserRegistrationComponent } from './Components/userRegistration/user-registration/user-registration.component';
 import { FormsModule } from '@angular/forms';
 import { CreatedMainteceAccountComponent } from './Components/UserRegistration/created-maintece-account/created-maintece-account.component';
-=======
 import { HttpClientModule } from '@angular/common/http';
->>>>>>> develop
+
 
 
 @NgModule({
@@ -21,11 +19,9 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-<<<<<<< HEAD
-    FormsModule
-=======
+    FormsModule,
     HttpClientModule
->>>>>>> develop
+
   ],
   providers: [],
   bootstrap: [AppComponent]
