eb10f81490181f17254f17d99156094cc5049b51
Mgonzalez06 <lisethgonzalez2000@gmail.com>
2022-03-29 11:56:21 -0600

    [new] view users

--- SICOP/src/app/Components/show-users/show-users.component.ts
+++ SICOP/src/app/Components/show-users/show-users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountManagementService } from '../../Services/account-management.service';
 
 @Component({
   selector: 'app-show-users',
@@ -7,9 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowUsersComponent implements OnInit {
 
-  constructor() { }
+  page:number=1;
+  userList:any[]=[];
+  maintanceUserList:any[]=[];
+  clientUserList:any[]=[];
+  
+  constructor(private _accountService:AccountManagementService) { }
 
   ngOnInit(): void {
+    this.loadUsers();
   }
+  loadUsers(){
+    this._accountService.getUsers().subscribe(
+      (data:any)=>{
+        this.userList= data.data.rows;
+        console.log(this.userList);
+      }
+    )
+  }
+  loadClientUsers(){
+    this._accountService.getClientUsers().subscribe(
+      (data:any)=>{
+        this.clientUserList= data.data.rows;
+        console.log(this.clientUserList);
+      }
+    )
+  }
+
+  loadMaintanceUsers(){
+    this._accountService.getMaintenanceUsers().subscribe(
+      (data:any)=>{
+        this.maintanceUserList= data.data.rows;
+        console.log(this.maintanceUserList);
+      }
+    )
+  }
+
+  inactiveUser(){
+
+  }
+
 
 }
--- SICOP/src/app/app-routing.module.ts
+++ SICOP/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreatedMainteceAccountComponent } from './Components/UserRegistration/created-maintece-account/created-maintece-account.component';
 import { UserRegistrationComponent } from './Components/userRegistration/user-registration/user-registration.component';
+import { ShowUsersComponent } from './Components/show-users/show-users.component';
 
 
 const routes: Routes = [
   {path: 'userRegistration', component: UserRegistrationComponent},
-  {path: 'MainteceRegistration', component: CreatedMainteceAccountComponent}
+  {path: 'MainteceRegistration', component: CreatedMainteceAccountComponent},
+  {path: 'showUsers',component:ShowUsersComponent}
 ]
 
 @NgModule({
