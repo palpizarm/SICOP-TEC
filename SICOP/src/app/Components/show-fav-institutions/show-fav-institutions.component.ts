1415df26169e9895cb825a7555a2f641d154a4cd
dcastro18 <h.davidcastro14@gmail.com>
2022-03-28 22:29:29 -0600

    Save Fav Institution Finished

--- SICOP/src/app/Components/show-fav-institutions/show-fav-institutions.component.ts
+++ SICOP/src/app/Components/show-fav-institutions/show-fav-institutions.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FavoriteInstitutionsManagementService } from 'src/app/Services/favorite-institutions-management.service';
 
 @Component({
   selector: 'app-show-fav-institutions',
@@ -7,9 +8,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowFavInstitutionsComponent implements OnInit {
 
-  constructor() { }
+  page:number = 1;
+
+  userID:number = 0;
+
+  favInstitutionsList:any = [{},{},{}]
+
+  institutionsList:any = []
+
+  constructor( private _favInstitutionsService : FavoriteInstitutionsManagementService
+  ) { }
 
   ngOnInit(): void {
+
+    //local storage
+
+    // this.loadFavInstitutions(this.userID);
+    // this.loadInstitutions(this.favInstitutionsList);
+
+    this.loadInstitutions();
+
+  }
+
+  loadFavInstitutions(userID:number){
+    this._favInstitutionsService.getFavorites(this.userID).subscribe(
+      (data:any) => {
+        this.favInstitutionsList = data.data.rows
+      }
+    )
+
+  }
+
+  loadInstitutions()
+  {
+    this._favInstitutionsService.getInstitutions().subscribe(
+      (data:any) => {
+        this.institutionsList = data.data.rows
+        console.log(this.institutionsList)
+      }
+    )
+  }
+
+  saveInstitution()
+  {
+    let institutionSelect = document.getElementById("institutionSelect") as HTMLInputElement;
+    let institutionName = institutionSelect.value;
+
+    this.institutionsList.forEach(i => {
+      if(i.name == institutionName)
+      {
+        console.log(i);
+        // this._favInstitutionsService.createFavorite(this.userID,i.institution_id).subscribe(
+        //   (data:any) => {
+        //     console.log(data);
+        //   }
+        // )
+
+      }
+      
+    });
+
   }
 
 }
