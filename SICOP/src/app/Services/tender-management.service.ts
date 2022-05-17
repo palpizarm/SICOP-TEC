468f8ec9b440e1bd03d50a0448441be669e84276
Fabian Martínez <54187391+fabianmartinez04@users.noreply.github.com>
2022-05-16 22:39:24 -0600

    [new] Services for TenderHistory methods.

--- SICOP/src/app/Services/tender-management.service.ts
+++ SICOP/src/app/Services/tender-management.service.ts
@@ -40,5 +40,30 @@ export class TenderManagementService {
 
     )
   }
+
+  insertHistory = (tender_id: number, user_id: number) => {
+    return this.http.post(
+      `${this.url}/insertHistory`,
+      {
+        "tender_id": tender_id,
+        "user_id": user_id
+      }
+    )
+  }
+
+  getHistory = (user_id: number) => {
+    return this.http.get(
+      `${this.url}/getHistory/${user_id}`
+    )
+  }
+
+  deleteHistory = (tender_ids: string[]) => {
+    return this.http.patch(
+      `${this.url}/deleteHistory`,
+      {
+        "tender_ids": tender_ids
+      }
+    )
+  }
   
 }
